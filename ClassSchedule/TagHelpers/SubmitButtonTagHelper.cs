31520858d4a5fb1c24243ac7c7d8886a92d27b0e
gwyn <vazgwyn@gmail.com>
2026-03-12 23:37:51 -0400

    Ch15

---
+++ ClassSchedule/TagHelpers/SubmitButtonTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace ClassSchedule.TagHelpers
+{
+    [HtmlTargetElement("button", Attributes = "type")]
+    public class SubmitButtonTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        { 
+            if (output.Attributes.TryGetAttribute("type", out TagHelperAttribute? attr))
+            {
+            string? typeValue = attr.Value?.ToString();
+        if (!string.IsNullOrEmpty(typeValue) &&
+            typeValue.Equals("submit", StringComparison.OrdinalIgnoreCase))
+            {
+        output.Attributes.AppendCssClass("btn");
+            output.Attributes.AppendCssClass("btn-dark");
+            }
+           }
+          }
+    }
+}
+//
