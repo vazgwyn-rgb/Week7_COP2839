6b9b5da6cc2fdecebe62a486a698bd9fb68c6be1
gwyn <vazgwyn@gmail.com>
2026-03-12 23:52:56 -0400

    Updated 15-1

---
+++ ClassSchedule/TagHelpers/MyLinkButtonTagHelper.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Routing; 
+
+namespace ClassSchedule.TagHelpers
+{
+    [HtmlTargetElement("my-link-button")]
+    public class MyLinkButtonTagHelper : TagHelper
+    {
+        private readonly LinkGenerator _linkGenerator;
+
+        public MyLinkButtonTagHelper(LinkGenerator linkGenerator)
+        {
+            _linkGenerator = linkGenerator;
+        }
+    public string? Action { get; set; }
+        public string? Controller { get; set; }
+        public string? Id { get; set; }
+        public bool IsActive { get; set; }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; } = default!;
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            string action = Action ??
+                ViewContext.RouteData.Values["action"]?.ToString() ?? string.Empty;
+
+            string controller = Controller ??
+                ViewContext.RouteData.Values["controller"]?.ToString() ?? string.Empty;
+
+            object? routeValues = Id == null ? null : new { id = Id };
+
+            string? url = _linkGenerator.GetPathByAction(
+                action,
+                controller,
+                routeValues
+                ) ?? "#";
+
+            string cssClasses = IsActive
+                ? "btn btn-dark"
+                : "btn btn-outline-dark";
+            output.BuildLink(url, cssClasses);
+        }
+    }
+}
